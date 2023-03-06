c496ddea1790e2a5a40776333c31e48e8528397f
Mateus Abel <mateusabel960@gmail.com>
2023-03-06 21:03:28 +0000

    done

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\curso;
 use Illuminate\Http\Request;
+Use Exception;
 
 class CursoController extends Controller
 {
@@ -13,7 +14,12 @@ class CursoController extends Controller
    }
 
    public function show($id){
-    return curso::findOrFail($id);
+      try{
+         return curso::findOrFail($id);
+      }catch(Exception $e){
+         return $e->getMessage();
+      }
+    
    }
 
    public function store(Request $request){
@@ -24,15 +30,25 @@ class CursoController extends Controller
    }
 
    public function update(Request $request, $id){
-    $Curso = curso::findOrFail($id);
-    $Curso->descricao = $request->descr;
-    $Curso->update();
+      try{
+         $Curso = curso::findOrFail($id);
+         $Curso->descricao = $request->descr;
+         $Curso->update()>0?"Atualizado com sucesso":"erro ao atualizar";
+      }catch(Exception $e){
+         return $e->getMessage();
+      }
+    
    }
 
    public function destroy($id){
+      try{
+         $Curso = curso::findOrFail($id);
+         $Curso->delete()>0?"Deletado com sucesso":"Nao encontrado";
+      }catch(Exception $e){
+         return $e->getMessage();
+      }
 
-    $Curso = curso::findOrFail($id);
-    $Curso->delete();
+    
 
    }
 }
